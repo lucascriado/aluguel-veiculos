31715d1535902aa5b52bfc47fb53c9be14fa63aa
lucascriado <lucascriado33@gmail.com>
2025-07-08 21:11:05 -0300

    style: shadcnui

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,8 +1,32 @@
-export default function Login() {
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/components/auth-form"
+
+export default function LoginPage() {
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Login</h1>
-      <p className="mt-4">Página Login</p>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Acme Inc.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="https://images.unsplash.com/photo-1698044048234-2e7f6c4e6aca?q=100&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
     </div>
   )
 }
